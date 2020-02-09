e941d1f8c8d5d11f1e253c5536362e70ea623305
leola90 <56285525+leola90@users.noreply.github.com>
2020-02-09 17:40:58 -0600

    Update script.js
    Updated jquery search to include some parameters.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 var settings = {
-    "url": "https://api.eventful.com/rest/events/search?keywords=shows&location=San+Diego&date=Today&app_key=3PCFhKqWmgV9xscv&",
+    "url": "https://api.eventful.com/json/events/search?&app_key=3PCFhKqWmgV9xscv&keywords=shows&location=houston&date=Today",
     "method": "GET",
     "timeout": 0,
   };
   
   $.ajax(settings).done(function (response) {
     console.log(response);
-    console.log(response.total_items);
+    console.log(response.events.event[0].title);
+    console.log(response.events.event[0].city_name);
+    console.log(response.events.event[0].region_name);
+    console.log(response.events.event[0].country_name);
+    console.log(response.events.event[0].start_time);
     
   });
-
-
